f12401e196137cb08ff68b1a02a62e8c1f3d8b39
joseph <127488089+kingberetta@users.noreply.github.com>
2025-02-04 12:15:02 +0000

    Create DoorCode.java

---
+++ DoorCode.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class DoorCode{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("INPUT YOUR SECURITY CODE: ");
+        String code = scan.nextLine();
+        if(code.equals("2772")){
+            System.out.println("Enter");
+        } else {
+            System.out.println("Unrecognised");
+        }
+    }
+}
